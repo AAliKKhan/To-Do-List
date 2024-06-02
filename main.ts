5683fd4928f587e8cf8bafd75ba4fc677e25d152
AAliKKhan <165782963+AAliKKhan@users.noreply.github.com>
2024-06-02 14:42:12 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+
+while (condition) {
+  let addTask = await inquirer.prompt([
+    {
+      name: "todo",
+      type: "input",
+      message: "What do you want to add in your todos?",
+      validate: function (input) {
+        if (input.trim() !== "") {
+          return true;
+        } else {
+          return "Please enter a valid todo.";
+        }
+      },
+    },
+    {
+      name: "addMore",
+      type: "confirm",
+      message: "Do you want to add more todos?",
+      default: false,
+    },
+  ]);
+  if (addTask.todo !== undefined) {
+    todos.push(addTask.todo);
+  }
+
+  condition = addTask.addMore;
+  console.log(todos);
+}
